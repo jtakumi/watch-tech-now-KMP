a0900369ab4d573d19a846cc293aee76b1f09536
jtakumi <takumi87jn@gmail.com>
2026-07-03 22:24:32 +0900

    Implement issue 3: MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/jtakumi/watchtechnow/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jtakumi/watchtechnow/MainActivity.kt
@@ -3,10 +3,19 @@ package com.jtakumi.watchtechnow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.jtakumi.watchtechnow.di.initKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!isKoinInitialized) {
+            initKoin()
+            isKoinInitialized = true
+        }
         setContent { App() }
+    }
+
+    private companion object {
+        var isKoinInitialized = false
     }
 }
